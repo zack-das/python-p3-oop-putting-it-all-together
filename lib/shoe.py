c0c4d599bc2750040348d0bd6a274db75d46d778
zack-das <izackdas@gmail.com>
2025-11-04 19:08:11 +0300

    Updates

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand,size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
